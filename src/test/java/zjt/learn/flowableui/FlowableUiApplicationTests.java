f943d43e9ed2134ce547dec789bf6190227b27d0
zhaojiatao <2322795718@qq.com>
2022-06-03 22:10:30 +0800

    Initial commit

---
+++ src/test/java/zjt/learn/flowableui/FlowableUiApplicationTests.java
@@ -0,0 +1,13 @@
+package zjt.learn.flowableui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
